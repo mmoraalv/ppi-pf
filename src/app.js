7c2185374b1180ea6d4ac48503ef429013a9e907
mariomora <m.mora.alv@gmail.com>
2023-09-08 11:17:54 -0600

    Se hacen cambios al app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ import { engine } from 'express-handlebars'
 import { Server } from 'socket.io'
 import { __dirname } from './path.js'
 import path from 'path'
-import routerProd from './routes/products.routes.js';
-import routerCart from './routes/cart.routes.js';
-import { ProductManager } from './controllers/productManager.js';
+
+//import routerCart from './routes/cart.routes.js';
+//import { ProductManager } from './controllers/productManager.js';
+import productRouter from './routes/products.routes.js'
+import mongoose from 'mongoose'
 
 const PORT = 8080;
 const app = express();
+
+mongoose.connect('mongodb+srv://mmoraalvz2:mma$223$@cluster0.a8nxh9t.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log("DB conectada"))
+    .catch((error) => console.log("Error en conexion a MongoDB Atlas: ", error))
+
 
 //Server
 const server = app.listen(PORT,()=>{
@@ -91,8 +98,8 @@ app.get('/static/chat', (req, res) => {
 	});
 });
 
-app.use('/api/products', routerProd);
-app.use('/api/carts', routerCart);
+//app.use('/api/carts', routerCart);
+app.use('/api/products', productRouter)
 
 //Multer
 app.post('/upload', upload.single('product'), (req, res) => {
@@ -100,23 +107,3 @@ app.post('/upload', upload.single('product'), (req, res) => {
     console.log(req.body)
     res.status(200).send("Imagen cargada")
 })
-
-
-/*import express from 'express'
-import userRouter from './routes/user.routes.js'
-import mongoose from 'mongoose'
-
-const app = express()
-const PORT = 4000
-
-mongoose.connect('mongodb+srv://mmoraalvz2:mma$223$@cluster0.a8nxh9t.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log("DB conectada"))
-    .catch((error) => console.log("Error en conexion a MongoDB Atlas: ", error))
-
-app.use(express.json())
-
-app.use('/api/users', userRouter)
-
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})*/
